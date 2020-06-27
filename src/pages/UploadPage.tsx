6456a7e9b51f9c9873d9ea6fd5d7029f45c9bf1c
garganjohn <garganjohn@gmail.com>
2020-06-27 13:20:01 -0400

    added very limited writing to firestore

--- src/pages/UploadPage.tsx
+++ src/pages/UploadPage.tsx
@@ -3,13 +3,26 @@ import { RouteComponentProps, useNavigate } from "@reach/router";
 import { db } from "../services/firebase";
 import { Unit } from "../models/Unit";
 
-interface Props extends RouteComponentProps {}
+interface Props extends RouteComponentProps { }
 
 export const UploadPage: React.FC<Props> = () => {
 
     const navigate = useNavigate();
 
-    return(
-        <p>filler text uploadpage</p>
+    useEffect(() => {
+
+    })
+    let data = "unit_3"
+
+    const pushUnit = async () => {
+        await db
+            .collection("units")
+            .add({data})
+    }
+
+
+    return (
+
+        <button onClick={pushUnit}>push unit</button>
     )
 }
